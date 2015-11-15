9d6b0171caa232fa086732787978465eb2ec88e6
Michael Washburn Jr <mdw7326@rit.edu>
2015-11-15 13:55:41 -0500

    Figured out how to test things
    SO MANY UNNECESSARY CLASS FILES UGH.
    But I'm too lazy to remove them rn.

--- Domination/test/junit/net/yura/domination/engine/RiskTest.java
+++ Domination/test/junit/net/yura/domination/engine/RiskTest.java
@@ -5,6 +5,7 @@
  */
 package net.yura.domination.engine;
 
+import net.yura.domination.engine.core.RiskGame;
 import junit.framework.TestCase;
 import net.yura.domination.ui.flashgui.MainMenu;
 
@@ -26,12 +27,87 @@ public class RiskTest extends TestCase {
         super.tearDown();
     }
     
-    public void testRenamePlayer() {
-        RiskUIUtil.parseArgs(new String[] {}); //new Risk() throws NPE without this
-        Risk risk = new Risk();
+    /* Helper Functions *******************************************************/
+    
+    /*
+     * Create a new risk game and encapsulate this stupid construction code.
+     */
+    private Risk NewRisk()throws InterruptedException{
+        RiskUIUtil.parseArgs(new String[] {});
+        final Risk risk = new Risk();
+        
+        /* add extra listeners used for testing */
+        risk.addRiskListener(new RiskAdapter() {
+            public void needInput(int s) {
+                synchronized (risk) {
+                        risk.notifyAll();
+                        }
+            }
+            public void noInput() {
+
+            }
+        } );
+        risk.parser("closegame");
+        syncGame(risk);
+        return risk;
+    }
+    
+    /*
+     * Add an assortment of players to the game.
+     */
+    private void addPlayers(Risk risk){
+        risk.parser("newplayer ai easy 6 6");
+        risk.parser("newplayer ai average 2 2");
+        risk.parser("newplayer ai hard 1 1");
+        risk.parser("newplayer ai easy 3 3");
+        risk.parser("newplayer ai average 4 4");
+        risk.parser("newplayer ai hard 5 5");
+    }
+    
+    /*
+     * Wait until the game processing catches up after making a new
+     * game, loading a game, or maybe just if weird things are happening
+     * in your tests you can't explain.
+     */
+    private void syncGame(Risk risk)throws InterruptedException{
+        synchronized (risk) {
+            while (risk.getGame() != null) {
+                risk.wait();
+            }
+	}
+    }
+    
+    /* Tests ******************************************************************/
+    
+    public void testRenamePlayer() throws InterruptedException{
+        final Risk risk = NewRisk();
     }
     
     //test processFromUI
+    
+    public void testLoadGame()throws InterruptedException{
+        //set up
+        final Risk risk = NewRisk();
+        
+        
+        //do stuff in the parser outside of the syncronized block
+        risk.parser("loadgame test/junit/res/game1.save");
+        
+        //Access Risk variables inside a syncronized block
+        synchronized (risk) {
+            //wait for the risk game to finish processing your actions
+            while (risk.getGame() == null) {
+                risk.wait();
+            }
+            //do your checks here
+            RiskGame game = risk.getGame();
+            assertEquals(game.getPlayers().size(), 4);
+        }
+        //tear down
+        risk.kill();
+	risk.join();
+    }
+    
     //loadgame, join, startserver, killserver
     
     //inGameParser
